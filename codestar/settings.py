3accb0341d597b26f71da9411b7b6f85edf72e51
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-07-24 14:45:37 +0000

    Heroku added, gunicorn installed, heroku allowed host and procfile

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-trmxs=_$^%-8(0(qicei+=lx1rd7g*&@1erd7s!5pmirndw=c7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-andrewodwyer-djangoblog-mz39txgmqvo.ws-eu115.gitpod.io']
+ALLOWED_HOSTS = ['8000-andrewodwyer-djangoblog-mz39txgmqvo.ws-eu115.gitpod.io','.herokuapp.com']
 
 
 # Application definition
