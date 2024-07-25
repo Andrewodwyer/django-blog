b6ff667f116246fc1bd15de9be1a36aadab130ff
Andrew O'Dwyer <andrewodwyer@yahoo.co.uk>
2024-07-25 14:51:44 +0000

    add env and secret key, also changed allowed host

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['8000-andrewodwyer-djangoblog-mz39txgmqvo.ws-eu115.gitpod.io','.herokuapp.com']
+ALLOWED_HOSTS = ['8000-andrewodwyer-djangoblog-r0tan7g0w8w.ws.codeinstitute-ide.net','.herokuapp.com']
 
 
 # Application definition
